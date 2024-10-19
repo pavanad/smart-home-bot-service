7a89ee48dd23edb5b65626a7be7ba89e526e1c4d
Adilson Pavan <adilson.pavan@sankhya.com.br>
2024-10-19 11:27:21 -0300

    chore: adding error message to service call

---
+++ app/messages.py
@@ -0,0 +1,5 @@
+ERROR_INVOKING_GRACE_SERVICE = (
+    "Ops, parece que algo deu errado por aqui. "
+    "Estou com alguns probleminhas no momento, mas já estou tentando resolver. "
+    "Pode tentar novamente daqui a pouco? 😊"
+)
