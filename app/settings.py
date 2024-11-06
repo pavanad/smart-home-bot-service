db18d6a569372929a534ca167f135d193f9a5366
Adilson Pavan <adilson.pavan@sankhya.com.br>
2024-11-06 00:05:44 -0300

    fix: change log mode

--- app/settings.py
+++ app/settings.py
@@ -11,8 +11,6 @@ def enable_logging():
         level=logging.INFO,
         format="%(asctime)s %(levelname)s (%(name)s) %(message)s",
         datefmt="%d-%m-%Y %H:%M:%S",
-        filename="logs/bot_service.log",
-        filemode="a",
     )
     logging.getLogger("httpx").setLevel(logging.WARNING)
 
